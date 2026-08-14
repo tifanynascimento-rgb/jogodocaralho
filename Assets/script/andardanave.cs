0344d204dea6b5964695140882796c3f34f18f96
TIFANY FERNANDA DE CASTRO NASCIMENTO <tifany_nascimento@estudante.sesisenai.org.br>
2026-08-13 21:57:34 -0300

    jogodocaralho

--- Assets/script/andardanave.cs
+++ Assets/script/andardanave.cs
@@ -1,15 +1,28 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class andardanave : MonoBehaviour
 {
+    Rigidbody2D rb;
+    public float thrustForce = 10f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+     if (Mouse.current.leftButton.isPressed)
+        {
+            Vector3 MousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.value);
+           Debug.Log("Mouse Position: " + MousePos);
 
+            Vector2 direction = MousePos - transform.position;
+            transform.up = direction;
+            rb.AddForce(direction * thrustForce);
+        }
     }
+
+}
--- Assets/script/meteorodapaixao.cs
+++ Assets/script/meteorodapaixao.cs
@@ -2,17 +2,27 @@ using UnityEngine;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
+    Rigidbody2D rb;
+
+    float minSpeed = 40f;
+
+    float maxSpeed = 90f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         float randomSize = Random.Range(1f, 3f);
         transform.localScale = new Vector3(randomSize, randomSize, 1); 
+        rb = GetComponent<Rigidbody2D>();
+        float RandomSpeed = Random.Range(minSpeed, maxSpeed);
+        Vector2 randomDirection = Random.insideUnitCircle;
+        rb.AddForce(randomDirection * RandomSpeed);
     }
 
     // Update is called once per frame
     void Update()
     {
         Random.Range(0, 1);
-        
+       
+       
     }
 }
