ce8dc018e4d067bae0cd59b774ce6f0d8e402315
TIFANY FERNANDA DE CASTRO NASCIMENTO <tifany_nascimento@estudante.sesisenai.org.br>
2026-08-20 21:48:14 -0300

    feito

--- Assets/script/andardanave.cs
+++ Assets/script/andardanave.cs
@@ -1,3 +1,4 @@
+using JetBrains.Annotations;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -5,23 +6,43 @@ public class andardanave : MonoBehaviour
 {
     Rigidbody2D rb;
     public float thrustForce = 10f;
+    public Animator animator;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        animator = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
+    private void naosei()
+    {
+        if (Mouse.current.leftButton.isPressed)
+        {
+            animator.SetBool("isMoving", true);
+        }
+        else
+        {
+            animator.SetBool("isMoving", false);
+        }
+    }
+ 
     void Update()
     {
      if (Mouse.current.leftButton.isPressed)
         {
             Vector3 MousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.value);
-           Debug.Log("Mouse Position: " + MousePos);
-
-            Vector2 direction = MousePos - transform.position;
+            Vector2 direction = (MousePos - transform.position). normalized;
             transform.up = direction;
             rb.AddForce(direction * thrustForce);
+        }
+        naosei();
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Colidiu com: " + collision.gameObject.tag);
+        if (collision.gameObject.CompareTag("meteoro"))
+        {
+            Destroy(gameObject);
         }
     }
 
